0083a3c82c7771c00c04a155c2527959cf6adfd9
AaUel-Knight27 <abelalex2098@gmail.com>
2026-04-20 18:41:36 +0300

    fix: reorder INSTALLED_APPS for cloudinary and staticfiles compatibility

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,13 +23,13 @@ DATABASES = {
 }
 
 INSTALLED_APPS = [
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'cloudinary_storage',
     'cloudinary',
     'rest_framework',
     'corsheaders',
